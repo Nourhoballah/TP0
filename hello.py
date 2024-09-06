3c5091f06aefa92a5424a7e8e919a19e62cf52c3
Nourhoballah <nourhoballah@outlook.com>
2024-09-06 12:47:28 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+math.pi
+x=math.pi*4**2
+print(x)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
-x=3
+print('Nour Hoballah')
+
